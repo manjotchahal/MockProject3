515221b6f32bf26394351f8c38b672a527f169e2
pjrivera08 <pj.rivera08@gmail.com>
2018-06-05 10:52:00 -0400

    added [Column(TypeName = "nvarchar(MAX)")] to string properties

--- MockProject3/MockProject3.DA/Models/Users.cs
+++ MockProject3/MockProject3.DA/Models/Users.cs
@@ -21,26 +21,31 @@ namespace MockProject3.DA.Models
 
         [Required(ErrorMessage = "Name is required")]
         [DataType(DataType.Text)]
+        [Column(TypeName = "nvarchar(MAX)")]
         [StringLength(100, ErrorMessage = "Name cannot be more than 100 characters")]
         public string Name { get; set; }
 
         [Required(ErrorMessage = "Address is required")]
         [DataType(DataType.Text)]
+        [Column(TypeName = "nvarchar(MAX)")]
         [StringLength(200, ErrorMessage = "Address cannot be more than 200 characters")]
         public string Address { get; set; }
 
         [Required(ErrorMessage = "Phone number is required")]
         [RegularExpression("[(]{1}[0-9]{3}[)]{1}[ ]{1}[0-9]{3}[-]{1}[0-9]{4}", ErrorMessage = "Format must be (###) ###-####")]
         [DataType(DataType.PhoneNumber)]
+        [Column(TypeName = "nvarchar(MAX)")]
         public string PhoneNumber { get; set; }
 
         [Required]
         [DataType(DataType.EmailAddress)]
+        [Column(TypeName = "nvarchar(MAX)")]
         [RegularExpression(".{1,200}[@].{1,200}[.].{1,5}", ErrorMessage = "Email is too long, max 200 character on each side of @")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Gender is required")]
         [DataType(DataType.Text)]
+        [Column(TypeName = "nvarchar(MAX)")]
         public string Gender { get; set; }
 
         DateTime Created { get; set; }
