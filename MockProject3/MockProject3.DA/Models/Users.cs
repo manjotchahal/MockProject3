ff9d79323839d8c039f2b283aa8c3d281b7381e3
pjrivera08 <pj.rivera08@gmail.com>
2018-06-05 10:19:33 -0400

    Added Users Model

---
+++ MockProject3/MockProject3.DA/Models/Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MockProject3.DA.Models
+{
+    public class Users
+    {
+        public int UserID { get; set; }
+        [Required]
+        public int BatchID { get; set; }
+        public int RoomID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Gender { get; set; }
+    }
+
+}
