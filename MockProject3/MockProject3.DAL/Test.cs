bd340dbf3228aae0febc760edb4b1fcde7b3f99a
Manjinder Singh <manjinder360@yahoo.com>
2018-06-05 14:18:57 -0400

    added class to test dbcontext

---
+++ MockProject3/MockProject3.DAL/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MockProject3.DAL.Models;
+
+namespace MockProject3.DAL
+{
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            ForecastDB1Context db = new ForecastDB1Context();
+            var room = new Rooms()
+            {
+                Address = "123 address st tampa, fl 33612",
+                Capacity = 6,
+                Gender = "Male",
+                Created = DateTime.Now,
+                Modified = DateTime.Now
+            };
+            db.Rooms.Add(room);
+            db.SaveChanges();
+        }
+    }
+}
