a7d21cd23d56473214b119cbb4908870e9962e8d
pjrivera08 <pj.rivera08@gmail.com>
2018-06-05 10:28:32 -0400

    Cleaned up Users Model

--- MockProject3/MockProject3.DA/Models/Users.cs
+++ MockProject3/MockProject3.DA/Models/Users.cs
@@ -1,26 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace MockProject3.DA.Models
 {
-    public class Users
+    [Table("User")]
+    public class Users 
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ScaffoldColumn(false)]
         public int UserID { get; set; }
+
         [Required]
         public int BatchID { get; set; }
+
         public int RoomID { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Name is required")]
+        [DataType(DataType.Text)]
+        [StringLength(100, ErrorMessage = "Name cannot be more than 100 characters")]
         public string Name { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Address is required")]
+        [DataType(DataType.Text)]
+        [StringLength(200, ErrorMessage = "Address cannot be more than 100 characters")]
         public string Address { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Phone number is required")
+        [RegularExpression("[(]{1}[0-9]{3}[)]{1}[ ]{1}[0-9]{3}[-]{1}[0-9]{4}", ErrorMessage = "Format must be (###) ###-####")]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
+
         [Required]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(".{1,200}[@].{1,200}[.].{1,5}", ErrorMessage = "Email is too long, max 200 character on each side of @")]
         public string Email { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Gender is required")]
         public string Gender { get; set; }
+
+        DateTime Created { get; set; }
+        DateTime Modified { get; set; }
     }
 
 }
