1a568e42daff03b2d4084caa6aace26a20fa5138
pjrivera08 <pj.rivera08@gmail.com>
2018-06-07 11:28:36 -0400

    Wiki got updated so model got updated

--- MockProject3/MockProject3.DA/Models/Batch.cs
+++ MockProject3/MockProject3.DA/Models/Batch.cs
@@ -14,30 +14,35 @@ namespace MockProject3.DA.Models
         [ScaffoldColumn(false)]
         public Guid Id { get; set; }
 
+        [Required]
         public Guid BatchId { get; set; }
 
         // The start date of the batch
         [DataType(DataType.DateTime)]
-        [Column(TypeName = "datetime")] // The column is of datetime
+        [Column(TypeName = "datetime2")] // The column is of datetime
         public DateTime StartDate { get; set; }
 
         [DataType(DataType.DateTime)]
-        [Column(TypeName = "datetime")] // The column is of datetime
-        public DateTime? EndDate { get; set; }
+        [Column(TypeName = "datetime2")] // The column is of datetime
+        public DateTime EndDate { get; set; }
 
         // Name of the batch
         [DataType(DataType.Text)]
+        [Required]
         [Column(TypeName ="nvarchar(MAX)")] // The column is nvarchar with MAX length possible in SQL
         public string BatchName { get; set; }
 
         // The total of the associates in the batch
+        [Range(0, 100)]
         public int BatchOccupancy { get; set; }
 
         // The Technology the batch will be learning
         [DataType(DataType.Text)]
+        [Required]
         [Column(TypeName = "nvarchar(MAX)")] // The column is nvarchar with MAX length possible in SQL
         public string BatchSkill { get; set; }
         
+        [Required]
         public Address Address { get; set; }
 
         [Column(TypeName = "datetime2")]
@@ -47,6 +52,6 @@ namespace MockProject3.DA.Models
         [Column(TypeName = "datetime2")]
         public DateTime Deleted { get; set; }
 
-        public virtual ICollection<Guid> UserIds { get; set; }
+        public virtual ICollection<User> Users { get; set; }
     }
 }
