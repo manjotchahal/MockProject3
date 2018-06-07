6789369cf5874c57d78aabef230dd5f61e288b73
Manjinder Singh <manjinder360@yahoo.com>
2018-06-07 11:57:42 -0400

    string isn't a valid type constraint

--- MockProject3/MockProject3.Api/Controllers/ForecastController.cs
+++ MockProject3/MockProject3.Api/Controllers/ForecastController.cs
@@ -144,7 +144,7 @@ namespace MockProject3.Api.Controllers
         /// Return a list of all Users in the database with the match search critiea.
         /// </return>
         // GET: api/forecast/Users/startDate/endDate/location
-        [HttpGet("Users/{startDate:datetime}/{endDate:datetime}/{location:string}")]
+        [HttpGet("Users/{startDate:datetime}/{endDate:datetime}/{location}")]
         public IActionResult Get(DateTime startDate, DateTime endDate, string location)
         {
             try
