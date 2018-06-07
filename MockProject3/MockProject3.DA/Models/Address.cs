cbe99a7c2dc07b10a949b662ba4119c150e138de
pjrivera08 <pj.rivera08@gmail.com>
2018-06-07 12:25:46 -0400

    Updated address model to match wiki

--- MockProject3/MockProject3.DA/Models/Address.cs
+++ MockProject3/MockProject3.DA/Models/Address.cs
@@ -11,24 +11,35 @@ namespace MockProject3.DA.Models
             [Key]
             [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
             public Guid Id { get; set; }
+            [Required]
             public Guid AddressId { get; set; }
             [DataType(DataType.Text)]
             [Column(TypeName = "nvarchar(MAX)")]
+            [Required]
+            [StringLength(255)]
             public string Address1 { get; set; }
             [DataType(DataType.Text)]
             [Column(TypeName = "nvarchar(MAX)")]
             public string Address2 { get; set; }
             [DataType(DataType.Text)]
             [Column(TypeName = "nvarchar(MAX)")]
+            [Required]
+            [StringLength(25)]
             public string City { get; set; }
             [DataType(DataType.Text)]
             [Column(TypeName = "nvarchar(MAX)")]
+            [Required]
+            [StringLength(2, MinimumLength=2)]
             public string State { get; set; }
             [DataType(DataType.Text)]
             [Column(TypeName = "nvarchar(MAX)")]
+            [Required]
+            [StringLength(5, MinimumLength=5)]
             public string PostalCode { get; set; }
             [DataType(DataType.Text)]
             [Column(TypeName = "nvarchar(MAX)")]
+            [Required]
+            [StringLength(2, MinimumLength=2)]
             public string Country { get; set; }
         }
 }
