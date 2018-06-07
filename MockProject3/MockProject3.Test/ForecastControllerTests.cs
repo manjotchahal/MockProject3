bac0b04961bfa1f780c63ab96d57dd0a45490a51
Kay Choi <kchoi@mail.sfsu.edu>
2018-06-07 11:27:56 -0700

    Added unit test project.

---
+++ MockProject3/MockProject3.Test/ForecastControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MockProject3.Test
+{
+    public class ForecastControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
