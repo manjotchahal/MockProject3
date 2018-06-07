a249a9c3cb850ced8c434a43b71a961acc1f99e6
Steven Nagy <geiganax@gmail.com>
2018-06-07 10:09:09 -0400

    code migrations applied via user/room models

---
+++ MockProject3/MockProject3.DA/Migrations/20180607133115_taketwo.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace MockProject3.DA.Migrations
+{
+    public partial class taketwo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Users",
+                type: "nvarchar(MAX)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "Rooms",
+                type: "nvarchar(MAX)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "Rooms");
+        }
+    }
+}
