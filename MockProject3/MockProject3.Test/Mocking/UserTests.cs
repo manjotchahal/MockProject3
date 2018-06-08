85275ed114d701dacd87b3b264032439a9fe17fe
Kay Choi <kchoi@mail.sfsu.edu>
2018-06-08 07:40:25 -0700

    Updated test class to reflect repository consolidation.

--- MockProject3/MockProject3.Test/Mocking/UserTests.cs
+++ MockProject3/MockProject3.Test/Mocking/UserTests.cs
@@ -7,15 +7,15 @@ using Microsoft.EntityFrameworkCore;
 
 using MockProject3.DA;
 using MockProject3.DA.Models;
-using MockProject3.DA.RepositoryPattern;
+using MockProject3.DA.Repos;
 using MockProject3.Api;
 
 namespace MockProject3.Test.Mocking
 {
     public class UserTests
     {
-        private Mock<ForecastContext> _mockContext;
-        private IRepository<User> _userRepository;
+        private Mock<IForecastContext> _mockContext;
+        private IRepo<User> _userRepository;
 
         [Fact]
         public void GetUsers_NoId_ReturnCollection()
