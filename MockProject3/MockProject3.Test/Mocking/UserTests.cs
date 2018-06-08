c1459ad2a5e85bd37129fa0a649ca8e756276486
Kay Choi <kchoi@mail.sfsu.edu>
2018-06-08 13:44:32 -0700

    Implemented UserRepo unit tests.

--- MockProject3/MockProject3.Test/Mocking/UserTests.cs
+++ MockProject3/MockProject3.Test/Mocking/UserTests.cs
@@ -16,14 +16,12 @@ namespace MockProject3.Test.Mocking
     {
         private ForecastContext _context;
         private IRepo<User> _userRepository;
+        private static DbContextOptions<ForecastContext> options;
 
         [Fact]
-        public void GetUsers_ReturnCollection()
+        public void Get_ReturnCollection()
         {
-            var options = new DbContextOptionsBuilder<ForecastContext>()
-                .UseInMemoryDatabase(databaseName: "InMemDb")
-                .Options;
-
+            init();
             using(_context = new ForecastContext(options)) {
                 // Arrange
                 IEnumerable<User> users;
@@ -40,16 +38,179 @@ namespace MockProject3.Test.Mocking
         }
 
         [Fact]
-        public void GetUsers_WithId_ReturnUser()
+        public void GetByDate_WithValidDate_ReturnNonEmptyCollection()
         {
-            // Arrange
+            init();
+            using(_context = new ForecastContext(options)) {
+                // Arrange
+                IEnumerable<User> users;
+                _userRepository = new UserRepo(_context);
+                _context.Users.Add(getTestUser());
+                _context.SaveChanges();
+                User user = _context.Users.FirstOrDefault();
 
-            // Act
+                // Act
+                users = _userRepository.GetByDate(user.Created);
 
-            // Assert
+                // Assert
+                Assert.Equal(users.FirstOrDefault().Id, user.Id);
+            }
+        }
+
+        [Fact]
+        public void GetByDate_WithInvalidDate_ReturnEmptyCollection() {
+            init();
+            using(_context = new ForecastContext(options)) {
+                // Arrange
+                IEnumerable<User> users;
+                _userRepository = new UserRepo(_context);
+                _context.Users.Add(getTestUser());
+                _context.SaveChanges();
+                User user = _context.Users.FirstOrDefault();
+
+                // Act
+                users = _userRepository.GetByDate(DateTime.MinValue);
+
+                // Assert
+                Assert.Empty(users);
+            }
+        }
+
+        [Fact]
+        public void GetLocations_ReturnCollection() {
+            init();
+            using(_context = new ForecastContext(options)) {
+                // Arrange
+                IEnumerable<String> locations;
+                _userRepository = new UserRepo(_context);
+                _context.Users.Add(getTestUser());
+
+                User user = getTestUser();
+                user.Location = "Tampa";
+                _context.Users.Add(user);
+
+                _context.SaveChanges();
+
+                // Act
+                locations = _userRepository.GetLocations();
+
+                // Assert
+                Assert.Equal(locations.Count(), 2);
+            }
+        }
+
+        [Fact]
+        public void GetBetweenDates_ValidDateRange_ReturnNonEmptyCollection() {
+            init();
+            using(_context = new ForecastContext(options)) {
+                // Arrange
+                IEnumerable<User> users;
+                _userRepository = new UserRepo(_context);
+                _context.Users.Add(getTestUser());
+                _context.SaveChanges();
+
+                // Act
+                users = _userRepository.GetBetweenDates(DateTime.Now, DateTime.Now);
+
+                // Assert
+                Assert.NotEmpty(users);
+            }
+        }
+
+        [Fact]
+        public void GetBetweenDates_InvalidDateRange_ReturnEmptyCollection() {
+            init();
+            using(_context = new ForecastContext(options)) {
+                // Arrange
+                IEnumerable<User> users;
+                _userRepository = new UserRepo(_context);
+                _context.Users.Add(getTestUser());
+                _context.SaveChanges();
+
+                // Act
+                users = _userRepository.GetBetweenDates(DateTime.MinValue, DateTime.MinValue);
+
+                // Assert
+                Assert.Empty(users);
+            }
+        }
+
+        [Fact]
+        public void GetBetweenDatesAtLocation_ValidDateRangeValidLocation_ReturnNonEmptyCollection() {
+            init();
+            using(_context = new ForecastContext(options)) {
+                // Arrange
+                IEnumerable<User> users;
+                _userRepository = new UserRepo(_context);
+                _context.Users.Add(getTestUser());
+                _context.SaveChanges();
+
+                // Act
+                users = _userRepository.GetBetweenDatesAtLocation(DateTime.Now, DateTime.Now, "Reston");
+
+                // Assert
+                Assert.NotEmpty(users);
+            }
+        }
+
+        [Fact]
+        public void GetBetweenDatesAtLocation_InvalidDateRangeValidLocation_ReturnEmptyCollection() {
+            init();
+            using(_context = new ForecastContext(options)) {
+                // Arrange
+                IEnumerable<User> users;
+                _userRepository = new UserRepo(_context);
+                _context.Users.Add(getTestUser());
+                _context.SaveChanges();
+
+                // Act
+                users = _userRepository.GetBetweenDatesAtLocation(DateTime.MinValue, DateTime.MinValue, "Reston");
+
+                // Assert
+                Assert.Empty(users);
+            }
+        }
+
+        [Fact]
+        public void GetBetweenDatesAtLocation_ValidDateRangeInvalidLocation_ReturnEmptyCollection() {
+            init();
+            using(_context = new ForecastContext(options)) {
+                // Arrange
+                IEnumerable<User> users;
+                _userRepository = new UserRepo(_context);
+                _context.Users.Add(getTestUser());
+                _context.SaveChanges();
+
+                // Act
+                users = _userRepository.GetBetweenDatesAtLocation(DateTime.Now, DateTime.Now, "test");
+
+                // Assert
+                Assert.Empty(users);
+            }
+        }
+
+        [Fact]
+        public void GetBetweenDatesAtLocation_InvalidDateRangeInvalidLocation_ReturnEmptyCollection() {
+            init();
+            using(_context = new ForecastContext(options)) {
+                // Arrange
+                IEnumerable<User> users;
+                _userRepository = new UserRepo(_context);
+                _context.Users.Add(getTestUser());
+                _context.SaveChanges();
+
+                // Act
+                users = _userRepository.GetBetweenDatesAtLocation(DateTime.MinValue, DateTime.MinValue, "test");
+
+                // Assert
+                Assert.Empty(users);
+            }
         }
 
         private void init() {
+            options = new DbContextOptionsBuilder<ForecastContext>()
+                .UseInMemoryDatabase(databaseName: "InMemDb")
+                .Options;
         }
 
         private User getTestUser() {
@@ -64,7 +225,9 @@ namespace MockProject3.Test.Mocking
                 Email = "test@test.com",
                 Gender = "M",
                 Type = "test",
-                UserId = Guid.NewGuid()
+                UserId = Guid.NewGuid(),
+                Created = DateTime.Now,
+                Deleted = DateTime.MaxValue
             };
             return result;
         }
