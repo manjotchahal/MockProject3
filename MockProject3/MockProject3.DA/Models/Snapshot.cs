5a756821a8f97b9aa438a43a6571d0022bfab8c2
Kay Choi <kchoi@mail.sfsu.edu>
2018-06-08 08:40:55 -0700

    Added DataAnnotations to Snapshot model.

--- MockProject3/MockProject3.DA/Models/Snapshot.cs
+++ MockProject3/MockProject3.DA/Models/Snapshot.cs
@@ -1,17 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace MockProject3.DA.Models
 {
     public class Snapshot
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [ScaffoldColumn(false)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Column(TypeName = "datetime2")]
         public DateTime Date { get; set; }
 
+        [Required]
         public int RoomCount { get; set; }
 
+        [Required]
         public int UserCount { get; set; }
 
+        [Required]
+        [DataType(DataType.Text)]
+        [Column(TypeName = "nvarchar(MAX)")]
         public string Location { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime Created { get; set; }
     }
 }
