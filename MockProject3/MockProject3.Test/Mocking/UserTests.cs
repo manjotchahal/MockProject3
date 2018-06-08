8a32b44418d538a8186b3232450aaac9041eac60
Kay Choi <kchoi@mail.sfsu.edu>
2018-06-07 22:20:08 -0700

    Renamed ForecastControllerTests to UserTests.

--- MockProject3/MockProject3.Test/Mocking/UserTests.cs
+++ MockProject3/MockProject3.Test/Mocking/UserTests.cs
@@ -1,14 +1,58 @@
 using System;
 using Xunit;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-namespace MockProject3.Test
+using MockProject3.DA;
+using MockProject3.DA.Models;
+using MockProject3.DA.RepositoryPattern;
+using MockProject3.Api;
+
+namespace MockProject3.Test.Mocking
 {
-    public class ForecastControllerTests
+    public class UserTests
     {
-        [Fact]
-        public void Test1()
-        {
+        private Mock<ForecastContext> _mockContext;
+        private IRepository<User> _userRepository;
 
+        [Fact]
+        public void GetUsers_NoId_ReturnCollection()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void GetUsers_WithId_ReturnUser()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        private void init() {
+        }
+
+        private User getTestUser() {
+            User result = new User{
+                Name = new Name {
+                    NameId = Guid.NewGuid()
+                },
+                Id = Guid.NewGuid(),
+                Location = "Reston",
+                Email = "test@test.com",
+                Gender = "M",
+                Type = "test",
+                UserId = Guid.NewGuid()
+            };
+            return result;
         }
     }
 }
